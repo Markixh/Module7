b4381ad6e41d904345fed27bc83191f5e5a87591
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-15 20:28:03 +0300

    Добавлено задание 7.1.10

---
+++ Task7,1,10/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task7_1_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        class BaseClass
+        {
+            protected string Name;
+
+            public BaseClass(string name)
+            {
+                Name = name;
+            }
+        }
+
+        class DerivedClass : BaseClass
+        {
+            public string Description;
+
+            public int Counter;
+
+            DerivedClass(string name, string description) : base(name)
+            {
+                Description = description;
+            }
+
+            DerivedClass(string name, string description, int counter) : base(name)
+            {
+                Description = description;
+                Counter = counter;
+            }
+        }
+    }
+}
