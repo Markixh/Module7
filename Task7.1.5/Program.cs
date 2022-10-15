f182501c530cd2f8dc07bb1bd530df477fb05a57
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-15 09:06:59 +0300

    Добавлено задание 7.1.5

---
+++ Task7.1.5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+
+namespace Task7_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        
+        class Plant
+        {
+            public int Size;
+        }
+
+        class Vegetable : Plant
+        {
+
+        }
+
+        class Berry : Plant
+        {
+
+        }
+
+        class Fruit : Plant
+        {
+
+        }
+
+        class Apple : Fruit
+        {
+
+        }
+
+        class Banana : Berry
+        {
+
+        }
+
+        class Pear : Fruit
+        {
+
+        }
+
+        class Potato : Vegetable
+        {
+
+        }
+
+        class Carrot :Vegetable
+        {
+
+        }        
+    }
+}
