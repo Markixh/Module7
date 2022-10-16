e8f13ba304962cf29dc0c592edb1619bb18bb349
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-16 14:52:54 +0300

    Добавлено задание 7.3.3

---
+++ Task7.3.3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task7_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public abstract class ComputerPart
+        {
+            public abstract void Work();
+        }
+
+        class Processor : ComputerPart
+        {
+            public override void Work()
+            {
+                Console.WriteLine("Процессор работает");
+            }
+        }
+
+        class MotherBoard : ComputerPart
+        {
+            public override void Work()
+            {
+                Console.WriteLine("Материнская карта работает");
+            }
+        }
+
+        class GraphicCard : ComputerPart
+        {
+            public override void Work()
+            {
+                Console.WriteLine("Видеокарта работает");
+            }
+        }
+    }
+}
