39e241e661338cda8ac8513c6856ce4ea1acc280
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-16 10:12:53 +0300

    Добавлено задание 7.2.12

---
+++ Task7.2.14/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task7_2_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        class IndexingClass
+        {
+            private int[] array;
+
+            public IndexingClass(int[] array)
+            {
+                this.array = array;
+            }
+
+            public int this[int i]
+            {
+                get 
+                {
+                    if (i >= 0 && i < array.Length)
+                        return array[i];
+                    else
+                        return 0;
+                }
+            }
+        }
+    }
+}
