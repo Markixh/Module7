3be148c72b65ed87cd51ccf3c95699dd6c3a34c6
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-17 21:12:23 +0300

    Добавлено задание 7.5.2

---
+++ Task7.5.2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task7_5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public class Obj
+        {
+            public string Name;
+            public string Description;
+            public static int MaxValue = 2000;
+        }
+    }
+}
