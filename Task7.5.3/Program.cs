65efb909cd4341ed1cca8c25056889c48be1e886
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-17 21:22:07 +0300

    Добавлено задание 7.5.3

---
+++ Task7.5.3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task7_5_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3;
+            int num2 = 58;
+
+            Helper.Swap(ref num1, ref num2);
+
+            Console.WriteLine(num1); //58
+            Console.WriteLine(num2); //3
+        }
+
+        public class Helper
+        {
+            public static void Swap(ref int x, ref int y)
+            {
+                var temp = x;
+                x = y;
+                y = temp;
+            }
+        }
+    }
+}
