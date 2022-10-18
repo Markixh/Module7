ec2eeac090c56a47af7596d78f511fac06693ea3
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-18 20:25:17 +0300

    Добавление задания 7.6.2

---
+++ Task7.6.2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task7_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car<ElectricEngine>();
+            Console.WriteLine("Hello, World!");
+        }
+
+        class Car<T>
+        {
+            public T Engine;
+        }
+
+        class ElectricEngine
+        {
+
+        }
+
+        class GasEngine
+        {
+
+        }
+    }
+}
