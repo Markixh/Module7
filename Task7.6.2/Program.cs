402af4123f5e8f9560927852e41983a6f1aa2c83
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-18 22:07:41 +0300

    Добавлено изменени в задание 7.6.2 с учетом задания 7.6.12

--- Task7.6.2/Program.cs
+++ Task7.6.2/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            var car = new Car<ElectricEngine>();
+            //var car = new Car<ElectricEngine>();
             Console.WriteLine("Hello, World!");
         }
 
-        class Car<TEngine> where TEngine : Engine
+        abstract class Car<TEngine> where TEngine : Engine
         {
             public TEngine Engine;
             public virtual void ChangePart<TPart>(TPart newPart) where TPart : Part { }
@@ -27,5 +27,20 @@
         class Differential : Part { }
 
         class Wheel : Part { }
+
+        class ElectricCar : Car<ElectricEngine> 
+        {
+            public override void ChangePart<TPart>(TPart newPart)
+            {
+                base.ChangePart(newPart);
+            }
+        }
+        class GasCar : Car<GasEngine> 
+        {
+            public override void ChangePart<TPart>(TPart newPart)
+            {
+                base.ChangePart(newPart);
+            }
+        }
     }
 }
