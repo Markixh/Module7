ab17bfcae1b02003709c21a6e9942dbbf224099b
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-18 20:34:04 +0300

    Добавлено задание 7.6.6

---
+++ Task7.6.6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task7_6_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        class Record<T, T1>
+        {
+            public T Id;
+            public T1 Value;
+            public DateTime Date;
+        }
+
+    }
+}
